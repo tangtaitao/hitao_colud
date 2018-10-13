3d59ab4e669318fdffda6dc25c712ea215b6c26d
tangtaitao <928607990@qq.com>
2018-10-13 10:16:39 +0800

    添加了订单模块中表的crud

---
+++ hitao_system/src/test/java/com/hzitxx/hitao/mapper/ShopOrderGoodsMapperTest.java
@@ -0,0 +1,108 @@
+package com.hzitxx.hitao.mapper;
+
+import static org.mockito.Matchers.longThat;
+import static org.mockito.Matchers.startsWith;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hzitxx.hitao.entity.ShopOrderGoods;
+import com.hzitxx.hitao.entity.ShopOrderLog;
+
+/**
+ * 测试ShopOrderGoodsMapper
+ * 
+ * @author Administrator
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopOrderGoodsMapperTest {
+
+	// 自动注入mapper
+	@Autowired
+	private ShopOrderGoodsMapper shopOrdergoodsMapper;
+
+	/**
+	 * 测试增加一条数据
+	 */
+	@Test
+	public void testAddShopOrderLog() {
+		ShopOrderGoods shopOrderGoods = new ShopOrderGoods();
+		shopOrderGoods.setBuyerId(1);
+		shopOrderGoods.setCommisRate(1);
+		shopOrderGoods.setCreatedTime(new Date());
+		shopOrderGoods.setGoodsId(1);
+		shopOrderGoods.setGoodsImage("1");
+		shopOrderGoods.setGoodsName("1");
+		shopOrderGoods.setGoodsNum(1);
+		shopOrderGoods.setGoodsPayPrice(1L);
+		shopOrderGoods.setGoodsPrice(new BigDecimal(1));
+		shopOrderGoods.setGoodsType(1);
+		shopOrderGoods.setOrderId(1);
+		shopOrderGoods.setPromotionsId(1);
+		int i = shopOrdergoodsMapper.addShopOrderGoods(shopOrderGoods);
+		System.out.println(i);
+	}
+
+	/**
+	 * 测试删除一条数据
+	 */
+	@Test
+	public void testDeleteById() {
+		int i = shopOrdergoodsMapper.deleteById(2);
+		System.out.println(i);
+	}
+
+	/**
+	 * 测试更新一条数据
+	 */
+	@Test
+	public void testUpdateById() {
+		ShopOrderGoods shopOrderGoods = new ShopOrderGoods();
+		shopOrderGoods.setBuyerId(2);
+		shopOrderGoods.setCommisRate(2);
+		shopOrderGoods.setGoodsId(2);
+		shopOrderGoods.setGoodsImage("2");
+		shopOrderGoods.setGoodsName("2");
+		shopOrderGoods.setGoodsNum(2);
+		shopOrderGoods.setGoodsPayPrice(2L);
+		shopOrderGoods.setGoodsPrice(new BigDecimal(2));
+		shopOrderGoods.setGoodsType(2);
+		shopOrderGoods.setOrderId(2);
+		shopOrderGoods.setPromotionsId(2);
+		shopOrderGoods.setIsDel(1);
+		shopOrderGoods.setRecId(1);
+		shopOrderGoods.setUpdatedTime(new Date());
+		int i = shopOrdergoodsMapper.updateById(shopOrderGoods);
+		System.out.println(i);
+	}
+
+	/**
+	 * 查询一条信息
+	 */
+	@Test
+	public void testFindOne() {
+		ShopOrderGoods shopOrderGoods = shopOrdergoodsMapper.findOne(1);
+		System.out.println(shopOrderGoods);
+	}
+
+	/**
+	 * 搜索数据
+	 */
+	@Test
+	public void testSreachShopOrderLog() {
+		Map<String, Object> map =new HashMap<>();
+		List<ShopOrderGoods> list = shopOrdergoodsMapper.searchShopOrderGoods(map);
+		System.out.println(list);
+	}
+}
---
+++ hitao_system/src/test/java/com/hzitxx/hitao/mapper/ShopTransportMapperTest.java
@@ -0,0 +1,85 @@
+package com.hzitxx.hitao.mapper;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hzitxx.hitao.SystemMicroService8083;
+import com.hzitxx.hitao.entity.ShopTransport;
+import com.hzitxx.hitao.mapper.ShopTransportMapper;
+/**
+ * 测试ShopTransportMapper
+ * @author Administrator
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SystemMicroService8083.class)
+public class ShopTransportMapperTest {
+	// 自动注入mapper对象
+	@Autowired
+	private ShopTransportMapper shopTransportMapper;
+
+	/**
+	 * 测试根据id查找一条数据
+	 */
+	@Test
+	public void testFindOne() {
+		ShopTransport shopTransport = shopTransportMapper.findOne(1);
+		System.out.println(shopTransport);
+	}
+
+	/**
+	 * 测试添加一条数据
+	 */
+	@Test
+	public void testAddShopTransport() {
+		ShopTransport shopTransport = new ShopTransport();
+		shopTransport.setTitle("c");
+		shopTransport.setSendTplId(1);
+		shopTransport.setIsDel(0);
+		shopTransport.setCreatedTime(new Date());
+		int i = shopTransportMapper.addShopTransport(shopTransport);
+		System.out.println(1);
+	}
+
+	/**
+	 * 测试根据id修改一条信息
+	 */
+	@Test
+	public void testUpdateById() {
+		ShopTransport shopTransport = new ShopTransport();
+		shopTransport.setId(3);
+		shopTransport.setTitle("c");
+		shopTransport.setSendTplId(1);
+		shopTransport.setIsDel(1);
+		shopTransport.setUpdatedTime(new Date());
+		int i = shopTransportMapper.updateById(shopTransport);
+		System.out.println(i);
+	}
+
+	/**
+	 * 测试删除一条信息
+	 */
+	@Test
+	public void testDeleteById() {
+		int i = shopTransportMapper.deleteById(3);
+		System.out.println(i);
+	}
+
+	/**
+	 * 测试查询信息
+	 */
+	@Test
+	public void testSearchShopTransport() {
+		Map<String, Object> map = new HashMap<>();
+		List<ShopTransport> list = shopTransportMapper.searchShopTransport(map);
+		System.out.println(list);
+	}
+}
