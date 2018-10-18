ca2166efc98259820d64ffe082863c9bca25c221
tangtaitao <928607990@qq.com>
2018-10-18 16:22:25 +0800

    添加了权限管理的部分功能

--- hitao_authc/src/main/java/com/hzitxx/hitao/service/privilege/ShopAdminService.java
+++ hitao_authc/src/main/java/com/hzitxx/hitao/service/privilege/ShopAdminService.java
@@ -11,43 +11,61 @@ public interface ShopAdminService {
 
 	/**
 	 * 新增用户信息
+	 * 
 	 * @return
 	 */
-	//public int addAdmin(ShopAdmin shopAdmin);
+	// public int addAdmin(ShopAdmin shopAdmin);
 	public ServerResponse<Integer> addAdmin(ShopAdmin shopAdmin);
+
 	/**
 	 * 修改用户信息
+	 * 
 	 * @return
 	 */
-	//public int updateAdmin(ShopAdmin shopAdmin);
+	// public int updateAdmin(ShopAdmin shopAdmin);
 	public ServerResponse<Integer> updateAdmin(ShopAdmin shopAdmin);
+
 	/**
 	 * 删除用户信息
+	 * 
 	 * @param adminId
 	 * @return
 	 */
-	//public int deleteAdmin(Integer adminId);
+	// public int deleteAdmin(Integer adminId);
 	public ServerResponse<Integer> deleteAdmin(Integer adminId);
+
 	/**
 	 * 查询所有用户
+	 * 
 	 * @param map
 	 * @return
 	 */
-	//public List<ShopAdmin> findAll(Map<String,Object> map);
-	public ServerResponse<LayuiEntity<ShopAdmin>> LayuiEntity(int page,int limit);
+	// public List<ShopAdmin> findAll(Map<String,Object> map);
+	public ServerResponse<LayuiEntity<ShopAdmin>> LayuiEntity(Map<String, Object> map,int page, int limit);
+
 	/**
 	 * 根据ID查询数据
+	 * 
 	 * @param adminId
 	 * @return
 	 */
-	//public List<ShopAdmin> findOne(Integer adminId);
+	// public List<ShopAdmin> findOne(Integer adminId);
 	public ServerResponse<List<ShopAdmin>> findOne(Integer adminId);
+
 	/**
 	 * 登录
+	 * 
 	 * @param adminName
 	 * @param adminPassword
 	 * @return
 	 */
-	public ServerResponse<Map<String,Object>> login(ShopAdmin shopAdmin);
+	public ServerResponse<Map<String, Object>> login(ShopAdmin shopAdmin);
 
+	/**
+	 * 检查用户名是否存在
+	 * 
+	 * @param adminName
+	 * @return
+	 */
+	public ServerResponse<Integer> checkExists(String adminName);
 }
