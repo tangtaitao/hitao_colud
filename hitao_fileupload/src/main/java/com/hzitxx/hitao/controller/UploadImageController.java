63d8a7eda4a28edbe73959d51867e88cc1ae7bb9
wrqk5608 <1259762170@qq.com>
2018-10-19 14:28:56 +0800

    kk

---
+++ hitao_fileupload/src/main/java/com/hzitxx/hitao/controller/UploadImageController.java
@@ -0,0 +1,76 @@
+package com.hzitxx.hitao.controller;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.PutObjectResult;
+import com.hzitxx.hitao.utils.ServerResponse;
+
+/**
+ * 图片上传控制层
+ * 
+ * @author wujun
+ *
+ */
+@RestController
+@Controller
+@RequestMapping("/uploadImage")
+public class UploadImageController {
+
+	/**
+	 * 文件上传中心
+	 */
+	@Value("${oss.endpoint}")
+	private String endPoint;
+
+	/**
+	 * oss账号
+	 */
+	@Value("${oss.accessKeyId}")
+	private String accessKeyId;
+
+	/**
+	 * oss密码
+	 */
+	@Value("${oss.accessKeySecret}")
+	private String accessKeySecret;
+
+	/**
+	 * 上传文件
+	 * 
+	 * @return
+	 */
+	@PostMapping("/uploadImages")
+	public ServerResponse<String> uploadImage(@RequestParam("file") MultipartFile file) {
+		if (file == null) {
+			return ServerResponse.createByErrorMessage("文件上传失败!");
+		}
+		String fileName = null;
+		try {
+			OSSClient client = new OSSClient("http://" + endPoint, accessKeyId, accessKeySecret);
+			// 上传文件
+			fileName = UUID.randomUUID().toString() + file.getOriginalFilename();
+			PutObjectResult result = client.putObject("hitao-images", fileName, file.getInputStream());
+			System.out.println(result.getETag());
+			System.out.println(result.getResponse()); // 获取响应结果
+			// "https://你的BucketName.你的Endpoint/自定义路径/" + fileName;
+			String url = "https://bucketName.endPoint/fileName";
+			url = url.replace("bucketName", "hitao-images");
+			url = url.replace("endPoint", endPoint);
+			url = url.replace("fileName", fileName);
+			return ServerResponse.createBySuccess("图片上传成功!", url);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ServerResponse.createByErrorMessage("文件上传失败！");
+		}
+	}
+}
