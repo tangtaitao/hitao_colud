1d078023ce2cc1078a1b0578f237d0632eda2c0d
wrqk5608 <1259762170@qq.com>
2018-10-19 14:42:20 +0800

    增加商品模块一些功能

---
+++ hitao_commons/src/main/java/com/hzitxx/hitao/utils/AttrArr.java
@@ -0,0 +1,21 @@
+package com.hzitxx.hitao.utils;
+
+/**
+ * 类目工具类
+ *
+ */
+public class AttrArr {
+
+    private String attrValue;
+    public void setAttrValue(String attrValue) {
+         this.attrValue = attrValue;
+     }
+     public String getAttrValue() {
+         return attrValue;
+     }
+	@Override
+	public String toString() {
+		return "AttrArr [attrValue=" + attrValue + "]";
+	}
+
+}
---
+++ hitao_commons/src/main/java/com/hzitxx/hitao/utils/ShopCategoryUtils.java
@@ -0,0 +1,30 @@
+package com.hzitxx.hitao.utils;
+import java.util.List;
+
+/**
+ * 类目工具类
+ *
+ */
+public class ShopCategoryUtils {
+
+    private List<AttrTemp> attrTemp;
+    private String catId;
+    public void setAttrTemp(List<AttrTemp> attrTemp) {
+         this.attrTemp = attrTemp;
+     }
+     public List<AttrTemp> getAttrTemp() {
+         return attrTemp;
+     }
+
+    public void setCatId(String catId) {
+         this.catId = catId;
+     }
+     public String getCatId() {
+         return catId;
+     }
+	@Override
+	public String toString() {
+		return "ShopCategoryUtils [attrTemp=" + attrTemp + ", catId=" + catId + "]";
+	}
+
+}
